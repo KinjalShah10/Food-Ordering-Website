0f90b1aadfa34de7b1627291bdbf1149a30fc359
KinjalShah10 <shahkinjal0813@gmail.com>
2024-06-05 13:45:49 +0530

    Home Page Added

--- Foodies/src/components/Banner.jsx
+++ Foodies/src/components/Banner.jsx
@@ -1,87 +1,93 @@
 import React from "react";
+import bannerImg from "/Images/home/banner.png";
 
 const Banner = () => {
   return (
-    <div className="section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
-      <div className="py-24 flex flex-col md:flex-row-reverse justify-between items-center gap-8">
-        {/* {image} */}
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%">
+      <div className="py-24 flex flex-col md:flex-row-reverse items-center justify-between gap-8">
+
+        {/* img */}
         <div className="md:w-1/2">
-          <img src="/Images/home/banner.png"></img>
+          <img src={bannerImg} alt="" />
           <div className="flex flex-col md:flex-row items-center justify-around -mt-14 gap-4">
-            <div className="flex bg-white py-2 px-3 rounded-2xl items-center gap-3 shadow-md w-64">
-              <img
-                src="/Images/recipes/b-food1.png"
-                alt=""
-                className="-rounded-2xl"
-              ></img>
+            <div className="bg-white px-3 py-2 rounded-2xl flex items-center gap-3 shadow-sm w-64">
+              <img src="/images/home/b-food1.png" alt=""  className="rounded-2xl"/>
               <div className="space-y-1">
-                <h5 className="font-medium mb-1">Spicy Noodles</h5>
+                <h5>Spicy noodles</h5>
                 <div className="rating rating-sm">
-                  <input
+              
+                <input
                     type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-yellow-500"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
+                    readOnly
+                  />
+                <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
+                    readOnly
                   />
                   <input
                     type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-yellow-500"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-yellow-500"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
                     checked
+                    readOnly
                   />
                   <input
                     type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2  bg-yellow-500"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-400"
                   />
                   <input
                     type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2  bg-yellow-500"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-400"
+                    readOnly
                   />
+                
                 </div>
                 <p className="text-red">$18.00</p>
               </div>
             </div>
-            <div className="sm:flex hidden bg-white py-2 px-3 rounded-2xl items-center gap-3 shadow-md w-64">
-              <img
-                src="/Images/recipes/b-food1.png"
-                alt=""
-                className="-rounded-2xl"
-              ></img>
+            <div className="bg-white px-3 py-2 rounded-2xl md:flex items-center gap-3 shadow-sm w-64 hidden">
+              <img src="/images/home/b-food1.png" alt=""  className="rounded-2xl"/>
               <div className="space-y-1">
-                <h5 className="font-medium mb-1">Spicy Noodles</h5>
+                <h5>Spicy noodles</h5>
                 <div className="rating rating-sm">
-                  <input
+                <input
                     type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-yellow-500"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
+                    readOnly
+                  />
+                <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
+                    readOnly
                   />
                   <input
                     type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-yellow-500"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2 bg-yellow-500"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
                     checked
+                    readOnly
                   />
                   <input
                     type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2  bg-yellow-500"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-400"
+                    readOnly
                   />
                   <input
                     type="radio"
-                    name="rating-2"
-                    className="mask mask-star-2  bg-yellow-500"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-400"
+                    readOnly
                   />
+                
                 </div>
                 <p className="text-red">$18.00</p>
               </div>
@@ -89,20 +95,20 @@ const Banner = () => {
           </div>
         </div>
 
-        {/* { texts} */}
-        <div className="md:w-1/2 space-y-7 px-4">
-          <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug text-black">
-            Dive into the tastes which your tounge says{" "}
-            <span className="text-green">Awesome</span>!!!
+        {/* texts */}
+        <div className="md:w-1/2 px-4 space-y-7">
+        <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
+            Dive into Delights Of Delectable <span className="text-green">Food</span>
           </h2>
-          <p className="text-xl text-[#4A4A4A]">
-            Where each plate weaves a story of culinary mastery and passionate
-            craftmanship.
+          <p className="text-[#4A4A4A] text-xl">
+            Where Each Plate Weaves a Story of Culinary Mastery and Passionate
+            Craftsmanship
           </p>
-          <button className="btn bg-green px-8 py-3 font-semibold text-white rounded-full">
-            Order
+          <button className="bg-green font-semibold btn text-white px-8 py-3 rounded-full">
+            Order Now
           </button>
         </div>
+        
       </div>
     </div>
   );
