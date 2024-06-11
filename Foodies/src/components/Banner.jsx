e752634703976bbf2ecd71b54f5852211614cdc1
KinjalShah10 <shahkinjal0813@gmail.com>
2024-06-11 10:43:17 +0530

    Banner added

--- Foodies/src/components/Banner.jsx
+++ Foodies/src/components/Banner.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import bannerImg from "/Images/home/banner.png";
+import bannerImg from "/Images/home/banner.jpg";
 
 const Banner = () => {
   return (
-    <div className="max-w-screen-2xl container mx-auto xl:px-24 bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%">
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 bg-gradient-to-r from-0% from-[#f7f2f225] to-[#272424] to-100%">
       <div className="py-24 flex flex-col md:flex-row-reverse items-center justify-between gap-8">
 
         {/* img */}
         <div className="md:w-1/2">
-          <img src={bannerImg} alt="" />
+          <img src={bannerImg} alt=""  className="rounded-xl"/>
           <div className="flex flex-col md:flex-row items-center justify-around -mt-14 gap-4">
             <div className="bg-white px-3 py-2 rounded-2xl flex items-center gap-3 shadow-sm w-64">
               <img src="/images/home/b-food1.png" alt=""  className="rounded-2xl"/>
@@ -54,7 +54,7 @@ const Banner = () => {
             <div className="bg-white px-3 py-2 rounded-2xl md:flex items-center gap-3 shadow-sm w-64 hidden">
               <img src="/images/home/b-food1.png" alt=""  className="rounded-2xl"/>
               <div className="space-y-1">
-                <h5>Spicy noodles</h5>
+                <h5>Passta</h5>
                 <div className="rating rating-sm">
                 <input
                     type="radio"
@@ -104,7 +104,7 @@ const Banner = () => {
             Where Each Plate Weaves a Story of Culinary Mastery and Passionate
             Craftsmanship
           </p>
-          <button className="bg-green font-semibold btn text-white px-8 py-3 rounded-full">
+          <button className="bg-black font-semibold btn text-white px-8 py-3 rounded-full">
             Order Now
           </button>
         </div>
